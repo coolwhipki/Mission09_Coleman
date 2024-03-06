7e6ef56e36225584eb439830b9deb6d55729afe6
coolwhipki <kirajanegirl@gmail.com>
2024-03-06 15:25:14 -0700

    Mission09 webpage with teams

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import TeamData from './CollegeBasketballTeams.json';
+
+//Interface
+interface CardProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1 className="App-kiraTouch">Welcome to March Maddness Season</h1>
+      <h2>Learn about info for the colleges in NCAA Basketball:</h2>
+      <br></br>
+    </div>
+  );
+}
+
+class Card extends React.Component<CardProps> {
+  render() {
+    const oneTeam = this.props;
+    return (
+      <div>
+        <h1>{oneTeam.school}</h1>
+        <h4>Mascot: {oneTeam.name}</h4>
+        <h4>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h4>
+        <br></br>
+        <p className="App-kiraTouch">-------------------------------</p>
+        <br></br>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {TeamData.teams.map((slay) => (
+        <Card {...slay} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Intro />
+      <TeamList />
     </div>
   );
 }
